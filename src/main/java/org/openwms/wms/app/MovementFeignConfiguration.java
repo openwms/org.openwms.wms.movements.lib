b4b50359a86d09fbcb816462ee93cd48fc3ab9b4
Heiko Scherrer <scherrer@openwms.org>
2020-04-28 22:58:47 +0200

    Do not depend on transport service but send events over Movements

--- src/main/java/org/openwms/wms/app/MovementFeignConfiguration.java
+++ src/main/java/org/openwms/wms/app/MovementFeignConfiguration.java
@@ -15,30 +15,24 @@
  */
 package org.openwms.wms.app;
 
-import org.ameba.mapping.BeanMapper;
-import org.ameba.mapping.DozerMapperImpl;
-import org.openwms.wms.impl.MovementRepository;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
+import org.openwms.common.location.api.LocationApi;
+import org.openwms.common.location.api.LocationGroupApi;
+import org.openwms.common.transport.api.TransportUnitApi;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Profile;
 
 /**
- * A MovementConfiguration.
+ * A MovementFeignConfiguration.
  *
  * @author Heiko Scherrer
  */
-@EnableJpaRepositories(basePackageClasses = MovementRepository.class)
-@EntityScan(basePackageClasses = MovementRepository.class)
-@EnableJpaAuditing
+@Profile("!INMEM")
 @Configuration
-class MovementConfiguration {
-
-    public
-    @Bean
-    BeanMapper beanMapper() {
-        return new DozerMapperImpl("META-INF/dozer/movements-bean-mappings.xml");
-    }
-
+@EnableFeignClients(basePackageClasses = {
+        TransportUnitApi.class,
+        LocationApi.class,
+        LocationGroupApi.class
+})
+class MovementFeignConfiguration {
 }
