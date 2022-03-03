6654f31cdcee753a480819e94c4a15c246adf478
Heiko Scherrer <hscherrer@interface21.io>
2022-03-03 22:36:16 +0100

    The RabbiTemplate must be extended with interceptors

--- src/main/java/org/openwms/wms/movements/app/MovementAsyncConfiguration.java
+++ src/main/java/org/openwms/wms/movements/app/MovementAsyncConfiguration.java
@@ -15,6 +15,7 @@
  */
 package org.openwms.wms.movements.app;
 
+import org.ameba.amqp.RabbitTemplateConfigurable;
 import org.openwms.core.SpringProfiles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,11 +31,14 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.amqp.support.converter.SerializerMessageConverter;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.retry.backoff.ExponentialBackOffPolicy;
 import org.springframework.retry.support.RetryTemplate;
@@ -70,9 +74,11 @@ class MovementAsyncConfiguration {
         return messageConverter;
     }
 
-    @Bean
-    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
-            MessageConverter messageConverter) {
+    @Primary
+    @Bean(name = "amqpTemplate")
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+            ObjectProvider<MessageConverter> messageConverter,
+            @Autowired(required = false) RabbitTemplateConfigurable rabbitTemplateConfigurable) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
         backOffPolicy.setMultiplier(2);
@@ -81,7 +87,10 @@ class MovementAsyncConfiguration {
         RetryTemplate retryTemplate = new RetryTemplate();
         retryTemplate.setBackOffPolicy(backOffPolicy);
         rabbitTemplate.setRetryTemplate(retryTemplate);
-        rabbitTemplate.setMessageConverter(messageConverter);
+        rabbitTemplate.setMessageConverter(messageConverter.getIfUnique());
+        if (rabbitTemplateConfigurable != null) {
+            rabbitTemplateConfigurable.configure(rabbitTemplate);
+        }
         return rabbitTemplate;
     }
 
